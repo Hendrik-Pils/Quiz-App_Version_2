f8f1aca4366201bdd7d4cf86b825d7dfd0b79833
Hendrik Pils <hendrik-pils@gmx.de>
2023-11-22 15:54:03 +0100

    initial changes

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // phew… not a lot going on here. Please add some code!
+
+/*const bodyElement = document.querySelector('[data-js="body"]', '[data-js="nav"]', '[data-js="header"]');
+
+
+const toggleButton = document.querySelector('[data-js="toggle__button"]');
+
+toggleButton.addEventListener("click", () => {
+  bodyElement.classList.toggle("dark");
+}); */
+
+
+const bookmarkButton = document.querySelector('[data-js="bookmark"]');
+
+bookmarkButton.addEventListener("click", () => {
+    bookmark.classList.toggle("bookmark--active");
+    console.log(bookmark.classList)
+});
